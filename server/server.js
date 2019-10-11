676bc6556eef6c94a84968867e8db75b306e5aec
Stephen Duke <stephen_duke@live.com>
2019-10-11 19:31:57 -0400

    Add shooting based on mouse position

--- server/server.js
+++ server/server.js
@@ -9,8 +9,19 @@ var server = http.Server(app);
 var io = socketIO(server);
 
 var move_speed = 5;
+var bullet_speed = 5;
+var bullet_delay_millis = 0;
+var bullet_size = 10;
 var refresh_rate = 1000/60;
+<<<<<<< Updated upstream
 var port_num = 5000;
+=======
+var port_num = process.env.PORT || 5000;
+
+var last_time = 0;
+
+const environment = process.env.ENV || "prod";
+>>>>>>> Stashed changes
 
 app.set('port', port_num);
 app.use('/static', express.static(__dirname + '/static'));
@@ -21,10 +32,15 @@ app.get('/', function(request, response) {
 });
 
 server.listen(port_num, function() {
+<<<<<<< Updated upstream
+=======
+  console.log(`Running as ${environment} environment`);
+>>>>>>> Stashed changes
   console.log('Starting server on port', port_num);
 });
 
 var players = {};
+var bullets = [];
 io.on('connection', function(socket) {
   socket.on('new player', function() {
     players[socket.id] = {
@@ -52,8 +68,34 @@ io.on('connection', function(socket) {
       player.y += move_speed;
     }
   });
+
+  //When player presses shoot key, mouse position is sent here, a new bullet is pushed to bullet array.
+  socket.on('shoot-bullet', function(angle){
+    if(players[socket.id] === undefined) return; //happens if server restarts
+    //Check time since last bullet
+    var curr_time = Date.now();
+    if (curr_time - last_time > bullet_delay_millis){
+      var x = Math.cos(angle) * 10;
+      var y = Math.sin(angle) * 10; 
+      var new_bullet = {x: players[socket.id].x + x, y: players[socket.id].y + y, time: curr_time, angle: angle, size: bullet_size};
+      bullets.push(new_bullet);
+      if(bullets.length > 100) bullets.shift();
+      last_time = curr_time;
+    }
+  });
 });
 
 setInterval(function() {
   io.sockets.emit('state', players);
+  for (var id in bullets) {
+    bullet = bullets[id];
+    bullet.x += Math.cos(bullet.angle) * bullet_speed;
+    bullet.y += Math.sin(bullet.angle) * bullet_speed; 
+    if(id === 0) console.log(bullet.x, bullet.y);
+    if (bullet.x > 800 || bullet.y > 600 || bullet.x < 0 || bullet.y < 0) {
+      bullets.shift();
+    }
+  }
+  io.sockets.emit('bullets-update', bullets);
 }, refresh_rate);
+
--- server/static/game.js
+++ server/static/game.js
@@ -1,7 +1,7 @@
-
-
 var socket = io();
 var refresh_rate = 1000/60;
+var mouse = {x: 0, y: 0};
+var angle = 0;
 
 var movement = {
     up: false,
@@ -10,8 +10,16 @@ var movement = {
     right: false
 };
 
+var bullet = false;
+
+
 socket.on('message', function(data){
     console.log(data);
+});
+
+document.addEventListener("mousemove", function(event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
 });
 
 document.addEventListener('keydown', function(event) {
@@ -27,6 +35,9 @@ document.addEventListener('keydown', function(event) {
         break;
       case 83: // S
         movement.down = true;
+        break;
+      case 32: // Space
+        bullet = true;
         break;
     }
   });
@@ -45,6 +56,9 @@ document.addEventListener('keyup', function(event) {
         case 83: // S
             movement.down = false;
             break;
+        case 32:
+            bullet = false;
+            break;
     }
 });
 
@@ -56,10 +70,11 @@ socket.on('connection', function(socket) {
   });
 });
 
-// update player movement to server
+// update player updates to server
 socket.emit('new player');
 setInterval(function() {
     socket.emit('movement', movement);
+    if(bullet) socket.emit('shoot-bullet', angle);
 }, refresh_rate);
 
 
@@ -68,13 +83,32 @@ canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
 socket.on('state', function(players) {
-  console.log(players);
+  //console.log(players);
   context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
+  context.fillStyle = 'blue';
   for (var id in players) {
     var player = players[id];
+    angle = Math.atan2(mouse.y-player.y,mouse.x-player.x);
     context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.arc(player.x, player.y, 12, 0,  2* Math.PI);
+    context.fill();
+    context.strokeStyle = 'purple';
+    context.beginPath();
+    context.lineWidth = 8;
+    context.arc(player.x,player.y,15,angle-(2*Math.PI)/12,angle+(2*Math.PI)/12);
+    context.stroke();
+    context.lineWidth = 1;
+  }
+});
+
+socket.on('bullets-update', function(bullets){
+  //console.log(bullets);
+  context.fillStyle = 'red';
+  for (var id in bullets) {
+    var bullet = bullets[id];
+    //console.log(bullet);
+    context.beginPath();
+    context.arc(bullet.x, bullet.y, bullet.size, 0, 2 * Math.PI);
     context.fill();
   }
 });
