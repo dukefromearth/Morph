022f6e185e3aa5deb5d33b23d3fcf75ffbe126fa
Matt <matthew.wong@outlook.com>
2019-11-05 03:31:27 -0500

    Added some game logic tests

---
+++ server/test/game.test.mjs
@@ -0,0 +1,54 @@
+'use strict'
+
+import chai from 'chai'
+import assert from 'assert'
+import Game from '../src/game.mjs'
+const expect = chai.expect
+
+
+describe('Game class', function(){
+	
+	const game = new Game(800,600);
+	const player_name = "test"
+
+
+	before((done) => {
+
+		done()
+
+	})
+
+	after((done) => {
+		
+		done()
+	})
+
+	it('Adding player', (done) => {
+		
+		game.new_player(player_name)
+		expect(Object.keys(game.players).includes(player_name)).to.be.true
+		done()
+			
+	})
+
+	it('Removing player', (done) => {
+		
+		game.delete_player(player_name)
+		expect(Object.keys(game.players).includes(player_name)).to.be.false
+		done()
+			
+	})
+
+	it('Reviving player', (done) => {
+		
+		game.revive_player(player_name)
+		expect(Object.keys(game.players).includes(player_name)).to.be.true
+		done()
+			
+	})
+
+
+
+	// it()
+	
+})
