28786ef91c7996bf19b439d3e756b9fb0dd95cb0
Stephen Duke <stephen_duke@live.com>
2019-11-19 16:21:51 -0500

    Update game.mjs

--- server/static/game.mjs
+++ server/static/game.mjs
@@ -43,77 +43,67 @@ canvas.addEventListener('touchstart', function(event){
   event.preventDefault();
   const touch = event.touches[1];
   const direction = updateDirection(touch.clientX, touch.clientY);
-  //down right
-  if (direction > Math.PI - 3*Math.PI/16 || direction < Math.PI/16){
-    movement.right = true;
-    movement.down = true;
-    movement.left = false;
-    movement.up = false;
-  }
-  //down
-  else if (direction > Math.PI - 3*Math.PI/16){
-    movement.right = false;
-    movement.down = true;
-    movement.left = false;
-    movement.up = false;
-  }
-  //down left
-  else if (direction > 3*Math.PI/8+Math.PI/16){
-    movement.right = false;
-    movement.down = true;
-    movement.left = true;
-    movement.up = false;
-  }
-  //left
-  else if (direction > 4*Math.PI/8+Math.PI/16){
-    movement.right = false;
-    movement.down = false;
-    movement.left = true;
-    movement.up = false;
-  }
-  //up left
-  else if (direction > 5*Math.PI/8+Math.PI/16){
-    movement.right = false;
-    movement.down = false;
-    movement.left = true;
-    movement.up = true;
-  }
-  //up
-  else if (direction > 6*Math.PI/8+Math.PI/16){
-    movement.right = false;
-    movement.down = false;
-    movement.left = false;
-    movement.up = true;
-  }
-  //up right
-  else if (direction > 7*Math.PI/8+Math.PI/16){
-    movement.right = true;
-    movement.down = false;
-    movement.left = false;
-    movement.up = false;
-  }
-  //right
-  else{
-    movement.right = true;
-    movement.down = false;
-    movement.left = false;
-    movement.up = false;
-  }
 
-  // if(touch.clientX < canvas.width/2-canvas.width/6) {
-  //   movement.left = true;
-  //   movement.right = false;
-  // }
-  // if(touch.clientX > canvas.width/2+canvas.width/6) {
-  //   movement.left = false;
+  if(direction < Math.PI/16  || direction > Math.PI-Math.PI/16){
+    movement.right = true;
+    movement.down = false;
+    movement.left = false;
+    movement.up = false;
+  }
+  //down right
+  // if (direction > Math.PI - 3*Math.PI/16 || direction < Math.PI/16){
   //   movement.right = true;
-  // }
-  // if(touch.clientY < canvas.height/2-canvas.height/6) {
-  //   movement.up = true;
-  //   movement.down = false;
-  // }
-  // if(touch.clientY > canvas.height/2+canvas.height/6) {
   //   movement.down = true;
+  //   movement.left = false;
+  //   movement.up = false;
+  // }
+  // //down
+  // else if (direction > Math.PI - 3*Math.PI/16){
+  //   movement.right = false;
+  //   movement.down = true;
+  //   movement.left = false;
+  //   movement.up = false;
+  // }
+  // //down left
+  // else if (direction > 3*Math.PI/8+Math.PI/16){
+  //   movement.right = false;
+  //   movement.down = true;
+  //   movement.left = true;
+  //   movement.up = false;
+  // }
+  // //left
+  // else if (direction > 4*Math.PI/8+Math.PI/16){
+  //   movement.right = false;
+  //   movement.down = false;
+  //   movement.left = true;
+  //   movement.up = false;
+  // }
+  // //up left
+  // else if (direction > 5*Math.PI/8+Math.PI/16){
+  //   movement.right = false;
+  //   movement.down = false;
+  //   movement.left = true;
+  //   movement.up = true;
+  // }
+  // //up
+  // else if (direction > 6*Math.PI/8+Math.PI/16){
+  //   movement.right = false;
+  //   movement.down = false;
+  //   movement.left = false;
+  //   movement.up = true;
+  // }
+  // //up right
+  // else if (direction > 7*Math.PI/8+Math.PI/16){
+  //   movement.right = true;
+  //   movement.down = false;
+  //   movement.left = false;
+  //   movement.up = false;
+  // }
+  // //right
+  // else{
+  //   movement.right = true;
+  //   movement.down = false;
+  //   movement.left = false;
   //   movement.up = false;
   // }
 })
