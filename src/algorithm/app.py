2108a4b5a0032650753bab1efa2bac945c6ff1b7
Matthew Wong <matthew.wong@outlook.com>
2019-12-14 18:15:36 -0500

    Added polling and function code.

--- src/algorithm/app.py
+++ src/algorithm/app.py
@@ -8,7 +8,7 @@ import time
 app = Flask(__name__)
 CORS(app)
 
-
+counter = 0
 # source ./flask_algo/bin/activate
 
 @app.route('/', methods=['POST'])
@@ -21,6 +21,23 @@ def home():
     print ('response', response)
     print ('time', end - start)
     return jsonify({"response":response})
+
+@app.route('/call', methods=['GET','POST'])
+def call():
+	global counter
+	counter += 1
+
+	# Replace this with your function
+	for i in range(1000000):
+		print(i)
+	# Replace end
+
+	counter -= 1
+	return "1"
+
+@app.route('/poll', methods=['GET'])
+def poll():
+	return str(counter)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
