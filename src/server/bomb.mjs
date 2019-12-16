aedb022e1b26884155847f96fa80c40a983adf51
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-12-16 17:01:44 -0500

    updated bomb
    bombs can be initialized with a starting point

--- src/server/bomb.mjs
+++ src/server/bomb.mjs
@@ -7,61 +7,97 @@
 
 export default class Bomb {
     constructor(posx,posy){
-        this.is_alive = bool;
-        this.pos = {x: posx, y: posy};
-        this.max_gens = 100;
-        this.gen_count = 0;
-        this.update_interval = 1000/120;
-        this.last_update = 0;
-        this.size = 300;
-        this.last_state = [];
-        this.next_state = [];
-        this.bomb_locations = [];
+    this.alive = false;
+    this.pos = { x: posx, y: posy };
+    this.max_gens = 70;
+    this.gen_count = 0;
+    this.update_interval = 1000 / 120;
+    this.last_update = 0;
+    this.size = 100;
+    this.last_state = [];
+    this.next_state = [];
+    this.bomb_locations = [];
+    this.running = false;
+
     }
-    update(){
-        var curr_time = Date.now();
-        if(curr_time - this.last_update > this.update_interval){
-            this.last_update = Date.now();
-            var bomb = [];
-            this.bomb_locations = [];
-            for(var i = 1; i < this.last_state.length-1; i++){
-                for (var j = 1; j <this.last_state.length-1; j++){
-                    var state = this.last_state;
-                    //count neighbors
-                    var neighbors = 0;
-                    if(this.last_state[i-1][j-1] === 1) neighbors++;//top left
-                    if(this.last_state[i][j-1] === 1) neighbors++;//top
-                    if(this.last_state[i+1][j-1] === 1) neighbors++;//top right
-                    if(this.last_state[i-1][j] === 1) neighbors++;//left
-                    if(this.last_state[i+1][j] === 1) neighbors++;//right
-                    if(this.last_state[i-1][j+1] === 1) neighbors++;//bottom left
-                    if(this.last_state[i][j+1] === 1) neighbors++;//bottom
-                    if(this.last_state[i+1][j+1] === 1) neighbors++;//bottom right
-                    if(this.last_state[i][j] === 0){
-                        if(neighbors === 3) {
-                            this.next_state[i][j] = 1;
-                            bomb = [this.pos.x+(i*5)-this.size*3,this.pos.y+(j*5)-this.size*3];
-                            this.bomb_locations.push(bomb);
-                        }
-                    } else {
-                        if(neighbors < 2) this.next_state[i][j] = 0;
-                        else if (neighbors > 3) this.next_state[i][j] = 0;
-                        else this.next_state[i][j] = 1;
-                    }
-                }
-            }
-            this.last_state = this.next_state;
-            this.gen_count++;
-            if(this.gen_count > this.max_gens) this.is_alive = false;
-        }
-    }
-    
-    initialize(starting_state){
+
+    run(starting_state) {
         this.last_state = Array(this.size).fill().map(() => Array(this.size).fill(0));
         this.next_state = Array(this.size).fill().map(() => Array(this.size).fill(0));
         for (let id in starting_state) {
             let state = starting_state[id];
             this.last_state[state.x][state.y] = 1;
         }
-    }  
+        for (let i = 0; i < this.max_gens; i++) {
+            this.update();
+            if(this.bomb_locations.length < 1) break;
+        }
+        return this.bomb_locations;
+    }
+    update(){
+        this.bomb_locations.length = 0;
+        this.next_state = Array(this.size).fill().map(() => Array(this.size).fill(0));
+        for (let j = 1; j < this.last_state.length - 1; j++) {
+            for (let i = 1; i < this.last_state.length - 1; i++) {
+                let bomb = { x: i, y: j };
+                //count neighbors
+                let neighbors = 0;
+                if (this.last_state[i - 1][j - 1] === 1) ++neighbors;//top left
+                if (this.last_state[i - 1][j] === 1) ++neighbors;//top
+                if (this.last_state[i - 1][j + 1] === 1) ++neighbors;//top right
+                if (this.last_state[i][j - 1] === 1) ++neighbors;//left
+                if (this.last_state[i][j + 1] === 1) ++neighbors;//right
+                if (this.last_state[i + 1][j - 1] === 1) ++neighbors;//bottom left
+                if (this.last_state[i + 1][j] === 1) ++neighbors;//bottom
+                if (this.last_state[i + 1][j + 1] === 1) ++neighbors;//bottom right
+                if (this.last_state[i][j] === 0) {
+                    if (neighbors === 3) {
+                        this.next_state[i][j] = 1;
+                        this.bomb_locations.push(bomb);
+                    }
+                } else {
+                    if (neighbors <= 1 || neighbors >= 4){
+                        this.next_state[i][j] = 0;
+                    }
+                    else {
+                        this.next_state[i][j] = 1;
+                        this.bomb_locations.push(bomb);
+                    }
+                }
+            }
+        }
+        this.last_state = this.next_state;
+        this.gen_count++;
+        if (this.gen_count > this.max_gens) this.is_alive = false;
+    }
+
+    init(starting_state) {
+        this.last_state = Array(this.size).fill().map(() => Array(this.size).fill(0));
+        this.next_state = Array(this.size).fill().map(() => Array(this.size).fill(0));
+        for (let id in starting_state) {
+            let state = starting_state[id];
+            this.last_state[state.x][state.y] = 1;
+        }
+    }
+    zero_out() {
+        for (let i in this.last_state) {
+            for (let j in this.last_state[i]) {
+                this.last_state[i][j] = 0;
+            }
+        }
+    }
+    randInit() {
+        for (var i = this.size / 2 - 3; i < this.size / 2 + 3; i++) {
+            for (var j = this.size / 2 - 3; j < this.size / 2 + 3; j++) {
+                var rawRandom = Math.random(); //get a raw random number
+                var improvedNum = (rawRandom * 2); //convert it to an int
+                var randomBinary = Math.floor(improvedNum);
+                if (randomBinary === 1) {
+                    this.last_state[i][j] = 1;
+                } else {
+                    this.last_state[i][j] = 0;
+                }
+            }
+        }
+    }
 }
