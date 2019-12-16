f753f6c7dd912ec0f76e95faaebd67a27f9ab92e
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-12-16 18:38:42 -0500

    finished direction
    direction funtion updated

--- src/server.mjs
+++ src/server.mjs
@@ -62,36 +62,36 @@ io.on('connection', function (socket) {
 
 });
 
-//up right pi/2 -> pi/4
+// right 
 var dir1_bank = [
   [[46,49],[51,49],[47,48],[50,51],[53,46],[48,49],[52,46],[53,52],[50,52],[47,53],[52,47],[49,52],[49,47],[48,52],[50,46],[48,53],[46,52],[51,48],[46,47],[50,50],[50,53]]
 ]
 
-//up right pi/4 -> 2pi
+// down right
 var dir2_bank = [
   [[48,51],[50,48],[49,51],[53,48],[53,49],[46,49],[46,46],[46,50],[53,50],[50,47],[52,53],[52,47],[51,49],[52,49],[46,52],[49,48],[48,48],[53,47],[50,46],[53,52],[48,47]]
 ]
-//right down 2pi -> 7pi/4
+// down
 var dir3_bank = [
   [[50,49],[46,51],[46,47],[49,53],[47,50],[52,50],[53,46],[53,47],[51,48],[51,53],[48,47],[46,48],[51,52],[49,50],[47,49],[48,51],[51,51],[49,47],[49,49],[48,46],[52,47]]
 ]
-//right down 7pi/4 -> 3pi/2
+// down left
 var dir4_bank = [
   [[49,49],[47,48],[49,50],[46,52],[48,46],[49,46],[51,52],[48,48],[48,49],[48,53],[50,53],[49,51],[51,53],[50,50],[49,52],[53,50],[52,50],[50,51]]
 ]
-//left down 3pi/2 -> 5pi/4
+// left 
 var dir5_bank = [
   [[49,48],[50,52],[52,46],[46,53][50,49],[51,48],[46,48],[51,46],[50,46],[48,49],[48,52],[48,51],[47,49],[49,50],[51,47],[47,50],[50,47],[52,49],[49,46],[53,48],[46,49]]
 ]
-//left down 5pi/4 -> pi
+// up left
 var dir6_bank =[
   [[48,52],[52,48],[52,51],[52,52],[48,48],[52,50],[51,47],[53,52],[47,53],[50,51],[48,46],[50,47],[47,51],[47,49],[46,52],[51,53],[52,49],[47,50],[46,48]]
 ]
-// left up pi -> 3pi/4
+// up
 var dir7_bank = [
   [[50,52],[46,47],[49,47],[52,49],[51,52],[50,51],[49,49],[49,51],[53,49],[51,49],[51,48],[47,53],[51,46],[52,53],[48,50],[46,49],[51,53],[52,50],[49,52],[53,46],[50,53]]
 ]
-// left up 3pi/4 -> pi/2
+// up right
 var dir8_bank = [
   [[52,46],[51,51],[53,46],[48,53],[48,52],[52,48],[51,49],[46,48],[49,53],[48,47],[47,52],[53,49],[47,50],[52,53],[50,52],[52,52],[48,49],[47,48],[53,48],[47,49],[51,47],[51,46]]
 ]
@@ -99,35 +99,35 @@ var dir8_bank = [
 function getStart(angle){
   //right
   if (angle > -Math.PI / 8 && angle < 0 || angle > 0 && angle < Math.PI / 8) {
-    //bomb init
+    game.bomb_init(dir1_bank[Math.floor(Math.random()*dir1_bank.length)]);
   }
   //down right
   else if (angle > Math.PI / 8 && angle < 3 * Math.PI / 8) {
-
+    game.bomb_init(dir2_bank[Math.floor(Math.random()*dir2_bank.length)]);
   }
   //down
   else if (angle > 3 * Math.PI / 8 && angle < 5 * Math.PI / 8) {
-
+    game.bomb_init(dir3_bank[Math.floor(Math.random()*dir3_bank.length)]);
   }
   //down left
   else if (angle > 5 * Math.PI / 8 && angle < 7 * Math.PI / 8) {
-
+    game.bomb_init(dir4_bank[Math.floor(Math.random()*dir4_bank.length)]);
   }
   //left
   else if (angle < Math.PI && angle > 7 * Math.PI / 8 || angle > -Math.PI && angle < -7 * Math.PI / 8) {
-
+    game.bomb_init(dir5_bank[Math.floor(Math.random()*dir5_bank.length)]);
   }
   //up left
   else if (angle > -7 * Math.PI / 8 && angle < -5 * Math.PI / 8) {
-
+    game.bomb_init(dir6_bank[Math.floor(Math.random()*dir6_bank.length)]);
   }
   //up
   else if (angle > -5 * Math.PI / 8 && angle < -3 * Math.PI / 8) {
-
+    game.bomb_init(dir7_bank[Math.floor(Math.random()*dir7_bank.length)]);
   }
   //up right
   else if (angle > -3 * Math.PI / 8 && angle < -Math.PI / 8) {
-
+    game.bomb_init(dir8_bank[Math.floor(Math.random()*dir8_bank.length)]);
   }
 }
 
